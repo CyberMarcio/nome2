11da46c04ba7b95a1603f3d58d4218e99c61fd15
marcio <viana_marcio@outlook.com>
2022-04-09 00:56:07 -0300

    First command lines with Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    // Ao utilizar var, é criado uma variável mutável
+    var usuario = "jamiltom damasceno"
+    usuario = "jose"
+
+    /* Ao utilizar val, é criado uma variável mutável*/
+    val pi = 3.14
+
+    var salario = 100
+    var bonus = 30
+
+    var total = salario + bonus
+
+    println("O salário é $total")
+
+
+
+}
